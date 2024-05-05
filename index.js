9e9bdb282a38bbbb65fc747d24a070b6a6c1d505
Kevin2118 <168947353+Kevin2118@users.noreply.github.com>
2024-05-04 22:11:52 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const yesBtn = document.querySelector('#yesBtn')
+
+yesBtn.addEventListener('click', function(){
+    alert('SABIA QUE DIRIAS QUE SI <3')
+    alert('ME HACES MUY FELIZ')
+})
+
+constnoBtn = document.querySelector('#noBtn')
+
+noBtn.addEventListener('mouseover', function(){
+    const randomX = parseInt(Math.random()*100)
+    const randomY = parseInt(Math.random()*100)
+
+    noBtn.style.setProperty('top', randomX+'%')
+    noBtn.style.setProperty('top', randomY+'%')
+
+    noBtn.style.setProperty('transform', 'translate(-${randomX}%, -${randomX}%,')
+})
